fdff2b44caebcdc69ecdcbd4d1e2d79a4fbad34c
atiblp <atiblpu@gmail.com>
2025-10-27 09:48:41 +0700

    fix: redesign bototm nav

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -2,18 +2,18 @@ import { Contact, HomeIcon, SearchIcon, Settings } from 'lucide-react'
 
 export default function BottomNav() {
     return (
-        <div className="bg-white flex  fixed -bottom-4 w-full h-20 rounded-t-4xl shadow-md  pb-20 justify-center gap-4">
-            <div className="flex-1 text-center max-w-11 h-11 outline-2 outline-white text-white -translate-y-1.5 rounded-full p-2 grid place-items-center bg-red-400">
+        <div className="bg-white pt-[2px] flex  fixed -bottom-4 w-full h-20 rounded-t-4xl shadow-sm  pb-20 justify-center gap-4">
+            <div className="flex-1 text-center max-w-11 h-11 outline-6 outline-white text-white -translate-y-1.5 rounded-full p-2 grid place-items-center bg-red-400">
                 <HomeIcon />
             </div>
-            <div className="flex-1 text-center max-w-11 h-11  rounded-full p-2 grid place-items-center text-gray-400">
+            <div className="flex-1 text-center max-w-11 h-11  rounded-full p-2 grid place-items-center text-gray-500">
                 <SearchIcon />
             </div>
-            <div className="flex-1 text-center max-w-11 h-11 text-gray-400 rounded-full p-2 grid place-items-center ">
+            <div className="flex-1 text-center max-w-11 h-11 text-gray-500 rounded-full p-2 grid place-items-center ">
                 <Contact />
             </div>
 
-            <div className="flex-1 text-center max-w-11 h-11 text-gray-400 rounded-full p-2 grid place-items-center ">
+            <div className="flex-1 text-center max-w-11 h-11 text-gray-500 rounded-full p-2 grid place-items-center ">
                 <Settings />
             </div>
         </div>
